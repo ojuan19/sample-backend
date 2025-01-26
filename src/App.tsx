8cf16e588c42d9c305bb355efa6525fb249d70d3
Juan Pablo osorio <64972735+ojuan19@users.noreply.github.com>
2025-01-26 10:05:29 -0500

    Reset repo and add my new files

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-center text-4xl font-extrabold text-blue-600">
-        Hello, World!
-      </h1>
+    <div className="text-center mt-4 text-xl">
+      Hello World
     </div>
   );
 }
