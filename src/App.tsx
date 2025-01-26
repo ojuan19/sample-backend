5a6fdfac7229efc8b9af774f3a708d47cbf64107
Juan Pablo osorio <64972735+ojuan19@users.noreply.github.com>
2025-01-26 09:17:03 -0500

    Reset repo and add my new files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Cart from './components/Cart';
-import ClientProfile from './components/ClientProfile';
-import Shipping from './components/Shipping';
-import Payment from './components/Payment';
-import { CheckoutProvider } from './context/CheckoutContext';
+import HelloWorld from './components/HelloWorld';
 
-const App: React.FC = () => {
+function App() {
   return (
-    <CheckoutProvider>
-      <Router>
-        <div className="min-h-screen bg-gray-100 p-5">
-          <Routes>
-            <Route path="/" element={<Cart />} />
-            <Route path="/client-profile" element={<ClientProfile />} />
-            <Route path="/shipping" element={<Shipping />} />
-            <Route path="/payment" element={<Payment />} />
-          </Routes>
-        </div>
-      </Router>
-    </CheckoutProvider>
+    <div className="App">
+      <HelloWorld />
+    </div>
   );
-};
+}
 
 export default App;
---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HelloWorld: React.FC = () => {
+  return (
+    <div className="text-center p-5">
+      <h1 className="text-xl font-bold">Hello, Vite + React + TypeScript + Tailwind CSS!</h1>
+    </div>
+  );
+};
+
+export default HelloWorld;
