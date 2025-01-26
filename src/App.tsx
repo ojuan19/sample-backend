0e14ed7a0c24e5424e286912ee6929f13beec588
Juan Pablo osorio <64972735+ojuan19@users.noreply.github.com>
2025-01-26 10:46:03 -0500

    Reset repo and add my new files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HelloWorld from './components/HelloWorld';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
+};
+
+export default App;
