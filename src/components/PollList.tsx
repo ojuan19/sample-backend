ed5279a4be871a36bfe9a16b9dccee98e7682c3f
Juan Pablo osorio <64972735+ojuan19@users.noreply.github.com>
2025-01-27 00:27:08 -0500

    Reset repo and add my new files

--- src/components/PollList.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const PollList = () => {
-  // TODO: Implement API call to fetch polls
-  return <div className="p-4 bg-white shadow rounded">List of Polls</div>;
-};
-
-export default PollList;
